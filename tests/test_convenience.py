fb3c5a536538f4bf546b4522be294f3d298d20dc
Vincent van de Schaft <vincentvdschaft@gmail.com>
2024-04-27 19:25:42 +0200

    Update

--- tests/test_convenience.py
+++ tests/test_convenience.py
@@ -5,22 +5,27 @@ import numpy as np
 
 from jaxus import simulate_to_usbmd
 from jaxus.containers import Medium, Probe, Pulse, Receive, Transmit
-from jaxus.plotting import plot_beamformed, plot_rf, plot_to_darkmode
+from jaxus.plotting import plot_beamformed, plot_rf, plot_to_darkmode, use_dark_style
 
 
 def test_simulate_to_usbmd():
+
+    n_el = 64
+    probe_geometry = np.stack(
+        [np.linspace(-9.5e-3, 9.5e-3, n_el), np.zeros(n_el)], axis=1
+    )
     probe = Probe(
-        probe_geometry=np.stack([np.linspace(-9e-3, 9e-3, 128), np.zeros(128)], axis=1),
+        probe_geometry=probe_geometry,
         center_frequency=5e6,
-        element_width=0.5e-3,
-        bandwidth=(0.5e6, 10e6),
+        element_width=probe_geometry[1, 0] - probe_geometry[0, 0],
+        bandwidth=(2e6, 9e6),
     )
     transmit = Transmit(
         t0_delays=np.zeros(probe.n_el),
         tx_apodization=np.ones(probe.n_el),
         waveform=Pulse(
             carrier_frequency=probe.center_frequency,
-            pulse_width=500e-9,
+            pulse_width=700e-9,
             chirp_rate=0.0,
             phase=0.0,
         ),
@@ -30,19 +35,20 @@ def test_simulate_to_usbmd():
         n_ax=1024,
         initial_time=0.0,
     )
-    n_scat = 50
+    n_scat = 10
+    scat_x = np.concatenate([np.linspace(-5e-3, 5e-3, 5), np.zeros(5)])
+    scat_y = np.concatenate([np.ones(5) * 15e-3, np.linspace(15e-3, 30e-3, 5)])
+
+    positions = np.stack([scat_x, scat_y], axis=1)
+
     medium = Medium(
-        scatterer_positions=np.stack(
-            [
-                np.random.randn(n_scat) * 4e-3,
-                np.abs(35e-3 + np.random.randn(n_scat) * 4e-3),
-            ],
-            axis=1,
-        ),
-        scatterer_amplitudes=np.ones(n_scat),
+        scatterer_positions=positions,
+        scatterer_amplitudes=np.ones(scat_x.shape[0]),
         sound_speed=1540,
     )
-    output_path = Path("tests", "output.h5")
+    output_path = Path(
+        r"C:\Users\vince\Documents\3_resources\data\verasonics\usbmd\simu\output.h5"
+    )
     # Remove the file if it exists
     if output_path.exists():
         output_path.unlink()
@@ -54,7 +60,10 @@ def test_simulate_to_usbmd():
         receive=receive,
         medium=medium,
     )
+    use_dark_style()
     fig, ax = plt.subplots()
-    plot_beamformed(ax, result[0], extent_m=[-19e-3, 19e-3, 30e-3, 0])
-    plot_to_darkmode(fig, ax)
+    depth = receive.n_ax / 2 / receive.sampling_frequency * medium.sound_speed
+    plot_beamformed(
+        ax, result[0], extent_m=[-19e-3, 19e-3, depth, 0], probe_geometry=probe_geometry
+    )
     plt.show()
